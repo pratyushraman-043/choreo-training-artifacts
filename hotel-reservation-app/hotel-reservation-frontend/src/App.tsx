83491e5148ab2be071617cb4ec8c8a4cc3f2e947
Pratyush Raman <pratyushraman@Pratyushs-Laptop.local>
2024-07-08 14:05:49 +0530

    Implement authentication functionality for the web    application

--- hotel-reservation-app/hotel-reservation-frontend/src/App.tsx
+++ hotel-reservation-app/hotel-reservation-frontend/src/App.tsx
@@ -16,7 +16,6 @@ import theme from "./theme";
 import ErrorPage from "./pages/error";
 import Cookies from "js-cookie";
 
-
 export default function App() {
   const [signedIn, setSignedIn] = useState(false);
   const [user, setUser] = useState<User>({
@@ -26,6 +25,7 @@ export default function App() {
     mobileNumber: "",
   });
   const [isAuthLoading, setIsAuthLoading] = useState(false);
+
   function getMappedUser(userInfo: any): User {
     return {
       email: userInfo?.email || "",
@@ -53,13 +53,57 @@ export default function App() {
       console.log("User is not signed in");
       if (
         window.location.pathname !== "/auth/login" &&
-        window.location.pathname !== "/") {
+        window.location.pathname !== "/"
+      ) {
         window.location.pathname = "/auth/login";
       }
     }
     setIsAuthLoading(false);
   }, []);
+
   if (isAuthLoading) {
     return <div>User authenticating...</div>;
   }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <UserContext.Provider value={user}>
+          <Header />
+          <ToastContainer />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              flexGrow: 1,
+            }}
+          >
+            <BrowserRouter>
+              <Routes>
+                {/* rooms */}
+                <Route path="/" Component={LandingPage} />
+                <Route path="/rooms" Component={RoomListing} />
+                {/* reservations */}
+                <Route path="/reservations" Component={ReservationListing} />
+                {/* new reservation */}
+                <Route
+                  path="/reservations/new"
+                  Component={ReservationAddingPage}
+                />
+                {/* update reservation */}
+                <Route
+                  path="/reservations/change"
+                  Component={ReservationUpdatingPage}
+                />
+                <Route path="/error" Component={ErrorPage} />
+                {/* Otherwise, show not found page */}
+                <Route path="*" Component={NotFound} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </UserContext.Provider>
+      </ThemeProvider>
+    </>
+  );
 }
--- hotel-reservation-app/hotel-reservation-frontend/src/layout/AppBar.tsx
+++ hotel-reservation-app/hotel-reservation-frontend/src/layout/AppBar.tsx
@@ -53,7 +53,6 @@ function UserMenu() {
           sessionStorage.removeItem("userInfo");
           window.location.href =
             `/auth/logout?session_hint=${Cookies.get('session_hint')}`;
-        
         }}>
           <Button style={{ textTransform: "none" }}>
             <Typography textAlign="center">My Reservations</Typography>
